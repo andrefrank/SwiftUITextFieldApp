8ece8958722b4194b4d664edd28836d5995272d3
Andre Frank <andrefrank.mail@t-online.de>
2019-11-16 16:46:54 +0100

    Initial Commit

---
+++ SwiftUITextFieldApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITextFieldApp
+//
+//  Created by Andre Frank on 16.11.19.
+//  Copyright © 2019 Afapps+. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
